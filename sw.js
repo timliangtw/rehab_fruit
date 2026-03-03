62601d4b1428f624100f767ea758d3c0d193735a
Tim Liang <tim.liang@adlinktech.com>
2026-03-03 15:41:20 +0800

    feat: Add PWA support by introducing manifest.json and sw.js, and integrating them into index.html.

---
+++ sw.js
@@ -0,0 +1,49 @@
+/* sw.js */
+const CACHE_NAME = 'rehab-fruit-v1';
+const urlsToCache = [
+    './index.html',
+    './',
+    './manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') return;
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+
+                const fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    response => {
+                        if (!response || response.status !== 200 || response.type !== 'basic' && response.type !== 'cors') {
+                            return response;
+                        }
+
+                        const responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                ).catch(error => {
+                    console.log('Fetch failed, maybe offline:', error);
+                });
+            })
+    );
+});
